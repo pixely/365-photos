30cc062914d1674722292359ebdbb83932d108eb
Graham Martin <graham@pixely.co.uk>
2017-03-24 20:48:46 +0000

    Add basic JS module structure for project

---
+++ src/js/components/common/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Spinner extends React.Component {
+  render() {
+    return (
+      <div>
+        Loading Spinner
+      </div>
+    );
+  }
+}
+
+export default Spinner;

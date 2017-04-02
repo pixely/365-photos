9db87e76f7030e67bc98cff0bdbf7e8d4735fcfa
Graham Martin <graham@pixely.co.uk>
2017-04-02 22:18:11 +0100

    Updating image styles

--- src/js/components/common/Image.js
+++ src/js/components/common/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Image extends React.Component {
   render() {
     return (
-      <img src="" alt="" />
+      <img src="https://scontent-lht6-1.cdninstagram.com/t51.2885-15/e35/16583181_1879147109036584_4217866375269449728_n.jpg" alt="" />
     );
   }
 }

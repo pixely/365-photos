9ddd7116d77e54c519bc9d43b61a4e7b348dc6a0
Graham Martin <graham@pixely.co.uk>
2017-04-04 00:06:30 +0100

    Initial refactor of fetch logic

--- src/js/actions/postActions.js
+++ src/js/actions/postActions.js
@@ -10,9 +10,22 @@ export function loadMediaSuccess(media) {
   return { type: types.LOAD_MEDIA_SUCCESS, media };
 }
 
-function loadMedia() {
-  // return fetch
+function loadMedia(dispatch, featured_media) {
+  fetch(endpoints.MEDIA_ENDPOINT + featured_media, {
+    method: 'get'
+  }).then(response => {
+    if (response.ok) {
+      response.json().then(data => {
+        dispatch(loadMediaSuccess(data));
+      });
+    } else {
+      throw("Looks like the response wasn't perfect, got status " +  response.status);
+    }
+  }).catch(error => {
+    throw(error);
+  });
 }
+
 export function loadPosts() {
   return function(dispatch) {
     dispatch(beginAjaxCall());
@@ -25,21 +38,10 @@ export function loadPosts() {
 
           data.map(post => {
             if (post.featured_media){
-              fetch(endpoints.MEDIA_ENDPOINT + post.featured_media, {
-                method: 'get'
-              }).then(response => {
-                if (response.ok) {
-                  response.json().then(data => {
-                    dispatch(loadMediaSuccess(data));
-                  });
-                } else {
-                  throw("Looks like the response wasn't perfect, got status " +  response.status);
-                }
-              }).catch(error => {
-                throw(error);
-              });
+              loadMedia(dispatch, post.featured_media);
             }
           });
+
         });
       } else {
         throw("Looks like the response wasn't perfect, got status " +  response.status);

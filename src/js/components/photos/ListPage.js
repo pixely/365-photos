cb9508867429a58aa4592724280fd942f52f4119
Graham Martin <graham@pixely.co.uk>
2017-04-07 12:28:22 +0100

    Monitor scroll position to update state when new image scrolls into view

--- src/js/components/photos/ListPage.js
+++ src/js/components/photos/ListPage.js
@@ -8,8 +8,51 @@ import * as postActions from '../../actions/postActions';
 // fetch from endpoint
 
 // set scroll position
+let page;
 
 class ListPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      current: null
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
+
+  }
+
+  componentDidMount() {
+    page = document.querySelector('.page');
+    page.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    page.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    let scrollTop = page.scrollTop;
+    let photos = Array.from(page.querySelectorAll('.photo'));
+
+    // TODO: Trigger URL change when reaching new photo
+    // TODO: Trigger colour change when half way to a new photo
+
+    let newState = photos
+      .map((photo, index) => {
+        if( scrollTop >= photo.offsetTop && scrollTop < (photo.offsetTop + photo.clientHeight) ){
+          return index;
+        }
+      })
+      .filter(Boolean)
+      .filter(photo => this.state.current !== photo )[0];
+
+    if( newState ) {
+      this.setState({current: newState});
+    }
+
+  }
+
   render() {
     return (
       <div className="page">
